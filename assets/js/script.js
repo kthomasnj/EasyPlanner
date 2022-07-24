7d37668bbd6765159cf25c103159012ba25ee7c6
Keith S Thomas <kthomasnj@hotmail.com>
2022-07-24 16:12:21 -0400

    Added hour rows to planner

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,37 @@
+var rootEl = $('.container')
 var timeEl = $('#currentDay');
-var weekDay = moment().format("dddd, MMMM Do");
-// var today = moment('[today] dddd');
+var currentTime = moment().format("dddd, MMMM Do");
+var hours = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
 
+function writeHourRows () {
+    for (let i = 0; i < hours.length; i++) {
+        var rowEl = $('<section>');        
+        var hourDiv = $('<div>');
+        var textAreaSection = $('<textarea>');
+        var rowBtn = $('<div>');    
+        var rowBtnImage = $('<img>');
 
+        // Create Row Element
+        rootEl.append(rowEl);
+        rowEl.attr('class', 'row');
 
-timeEl.text(weekDay);
+        // Create Hour column
+        rowEl.append(hourDiv);
+        hourDiv.text(hours[i]);
+        hourDiv.attr('class', 'hour');
+
+        // Create Calendar Event Field
+        rowEl.append(textAreaSection);
+
+        // Create Save Button
+        rowEl.append(rowBtn);
+        rowBtn.attr('class', 'saveBtn');
+
+        // Append Image to Button DIV
+        rowBtn.append(rowBtnImage);
+        rowBtnImage.attr('src', './assets/images/save.png');
+    }
+}
+
+timeEl.text(currentTime);
+writeHourRows();
