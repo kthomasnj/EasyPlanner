77aebca4af756e4d8b6adf7b87532cde7f0916ad
Keith S Thomas <kthomasnj@hotmail.com>
2022-07-26 16:28:57 -0400

    added field coloring logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,30 @@ var timeEl = $('#currentDay');
 var saveBtn = $('.savebtn');
 var currentTime = moment().format("dddd, MMMM Do");
 var currentTimeHr = moment().format("ha");
-var hours = [moment.unix(8000000).format("ha"), moment.unix(5500000).format("ha"), moment.unix(1700000).format("ha"), moment.unix(2050000).format("ha"), moment.unix(1450000).format("ha"), moment.unix(7500000).format('ha'), moment.unix(5000000).format('ha'), moment.unix(1200000).format("ha"), moment.unix(9500000).format("ha")];
+var hours = [moment("1-1-2027 09:00:00").format("ha"), moment("1-1-2027 10:00:00").format("ha"), moment("1-1-2027 11:00:00").format("ha"), moment("1-1-2027 12:00:00").format("ha"), moment("1-1-2027 13:00:00").format("ha"), moment("1-1-2027 14:00:00").format("ha"), moment("1-1-2027 15:00:00").format("ha"), moment("1-1-2027 16:00:00").format("ha"), moment("1-1-2027 17:00:00").format("ha")];
 
-function writeHourRows () {
+function writeHourRows() {
     for (let i = 0; i < hours.length; i++) {
-        var rowEl = $('<section>');        
+        var rowEl = $('<section>');
         var hourDiv = $('<div>');
         var textAreaSection = $('<textarea>');
-        var rowBtn = $('<div>');    
+        var rowBtn = $('<div>');
         var rowBtnImage = $('<img>');
-        var hourRowStyle = hours[i] < currentTimeHr ? "past" : hours[i] == currentTimeHr ? "present" : "future";
+
+         // Use IF-Then statement to change row color
+        if (hours[i] === currentTimeHr) {
+            hourRowStyle = "present";
+        } else if (hours[i] > currentTimeHr) {
+            hourRowStyle = "future";
+        } else {
+            hourRowStyle = "past";
+        }
+
+        console.log(hours[i] + "=" + currentTimeHr + ":" + hourRowStyle);
+
+        // console.log(str);
+
+        // hours[i] < currentTimeHr ? "past" : hours[i] == currentTimeHr ? "present" : "future";
 
         // Create Row Element
         rootEl.append(rowEl);
@@ -26,10 +40,12 @@ function writeHourRows () {
         // Create Calendar Event Field        
         rowEl.append(textAreaSection);
         textAreaSection.attr('class', hourRowStyle);
+        textAreaSection.attr('id', hours[i]);
 
         // Create Save Button
         rowEl.append(rowBtn);
         rowBtn.attr('class', 'saveBtn');
+        rowBtn.attr('data-index', hours[i]);
 
         // Append Image to Button DIV
         rowBtn.append(rowBtnImage);
@@ -37,8 +53,13 @@ function writeHourRows () {
     }
 }
 
-rootEl.click(function () {
-    console.log(timeEl);
+rootEl.click(function (event) {
+    var element = event.target;
+
+    if (element.matches("img") === true) {
+        var index = element.parentElement.getAttribute("data-index");
+        var meetingDes = $("#" + index).get(0);
+    }
 })
 
 timeEl.text(currentTime);
