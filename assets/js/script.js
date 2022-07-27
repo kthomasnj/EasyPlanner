6e6a055a61e2a340171f4e0a44d6347ac836e155
Keith S Thomas <kthomasnj@hotmail.com>
2022-07-27 08:03:09 -0400

    Corrected past future present textarea color formatting

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,11 @@ var rootEl = $('.container')
 var timeEl = $('#currentDay');
 var saveBtn = $('.savebtn');
 var currentTime = moment().format("dddd, MMMM Do");
-var currentTimeHr = moment().format("ha");
-var hours = [moment("1-1-2027 09:00:00").format("ha"), moment("1-1-2027 10:00:00").format("ha"), moment("1-1-2027 11:00:00").format("ha"), moment("1-1-2027 12:00:00").format("ha"), moment("1-1-2027 13:00:00").format("ha"), moment("1-1-2027 14:00:00").format("ha"), moment("1-1-2027 15:00:00").format("ha"), moment("1-1-2027 16:00:00").format("ha"), moment("1-1-2027 17:00:00").format("ha")];
+var currentTimeHr = moment().format("HH");
+var curentTimeHrInt = moment().format("h");
+var hours = [moment("1-1-2027 08:00:00").format("ha"),moment("1-1-2027 09:00:00").format("ha"), moment("1-1-2027 10:00:00").format("ha"), moment("1-1-2027 11:00:00").format("ha"), moment("1-1-2027 12:00:00").format("ha"), moment("1-1-2027 13:00:00").format("ha"), moment("1-1-2027 14:00:00").format("ha"), moment("1-1-2027 15:00:00").format("ha"), moment("1-1-2027 16:00:00").format("ha"), moment("1-1-2027 17:00:00").format("ha"), moment("1-1-2027 18:00:00").format("ha")];
+var hoursInts = [moment("1-1-2027 08:00:00").format("HH"), moment("1-1-2027 09:00:00").format("HH"), moment("1-1-2027 10:00:00").format("H"), moment("1-1-2027 10:00:00").format("HH"), moment("1-1-2027 11:00:00").format("HH"), moment("1-1-2027 12:00:00").format("HH"), moment("1-1-2027 13:00:00").format("HH"), moment("1-1-2027 14:00:00").format("HH"), moment("1-1-2027 15:00:00").format("HH"), moment("1-1-2027 16:00:00").format("HH"), moment("1-1-2027 17:00:00").format("HH"), moment("1-1-2027 18:00:00").format("HH")];
+
 
 function writeHourRows() {
     for (let i = 0; i < hours.length; i++) {
@@ -13,16 +16,18 @@ function writeHourRows() {
         var rowBtn = $('<div>');
         var rowBtnImage = $('<img>');
 
+        console.log(currentTimeHr);
+
          // Use IF-Then statement to change row color
-        if (hours[i] === currentTimeHr) {
+        if (hoursInts[i] === currentTimeHr) {
             hourRowStyle = "present";
-        } else if (hours[i] > currentTimeHr) {
+        } else if (hoursInts[i] < curentTimeHrInt) {
             hourRowStyle = "future";
         } else {
             hourRowStyle = "past";
         }
 
-        console.log(hours[i] + "=" + currentTimeHr + ":" + hourRowStyle);
+        console.log(hoursInts[i] + "=" + currentTimeHr + ":" + hourRowStyle);
 
         // console.log(str);
 
@@ -58,7 +63,9 @@ rootEl.click(function (event) {
 
     if (element.matches("img") === true) {
         var index = element.parentElement.getAttribute("data-index");
-        var meetingDes = $("#" + index).get(0);
+        var meetingDes = $("#" + index).get(0).value;
+
+        localStorage.setItem(time[index], meetingDes);
     }
 })
 
